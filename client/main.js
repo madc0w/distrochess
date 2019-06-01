a01e0c8f3481fefb1b9a7e6f76f785bdc2cf91b9
Marc A. Donis <mad7@runbox.com>
2019-06-01 17:45:48 +0200

    .

--- client/main.js
+++ client/main.js
@@ -2,3 +2,48 @@ import { Template } from "meteor/templating";
 import { ReactiveVar } from "meteor/reactive-var";
 
 import "./main.html";
+
+Template.main.onRendered(() => {
+	console.log("main rendered");
+
+	const position = {
+		a8 : 'bR',
+		c8 : 'bB',
+		g8 : 'bK',
+		g7 : 'bP',
+		a6 : 'wP',
+		c6 : 'bN',
+		e6 : 'bP',
+		f6 : 'bR',
+		h6 : 'bP',
+		a5 : 'bQ',
+		c5 : 'bP',
+		b4 : 'bB',
+		d4 : 'wP',
+		c3 : 'wN',
+		f3 : 'wN',
+		a2 : 'wP',
+		b2 : 'wP',
+		d2 : 'wQ',
+		e2 : 'wB',
+		f2 : 'wP',
+		g2 : 'wP',
+		h2 : 'wP',
+		a1 : 'wR',
+		e1 : 'wK',
+		h1 : 'wR'
+	};
+
+	const cfg = {
+		draggable : true,
+		dropOffBoard : "snapback", // this is the default
+		position : position,
+	//		onDrop : onDrop
+	};
+
+	const board = new ChessBoard("chess-board", cfg);
+	//	if (!board.whiteToMove) {
+	//		board.flip();
+	//	}
+
+});
