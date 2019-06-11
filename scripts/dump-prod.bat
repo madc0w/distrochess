d2c0de1b58ac5a9547cb4a651cc1bbf12d5052af
Marc A. Donis <mad7@runbox.com>
2019-06-11 18:50:26 +0200

    .

---
+++ scripts/dump-prod.bat
@@ -0,0 +1,1 @@
+mongodump --host mongodb-1650-0.cloudclusters.net --port 10005 --username distrochess --password blimey --out prod  --authenticationDatabase admin --db distrochess 
---
+++ scripts/restore-prod-to-local.bat
@@ -0,0 +1,1 @@
+mongorestore --host localhost:3001 --db meteor --drop prod\distrochess
