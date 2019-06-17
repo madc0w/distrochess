5b2a194b44cbdbf5c661a1f8a0b095c0f27c37bb
Marc A. Donis <mad7@runbox.com>
2019-06-17 18:58:40 +0200

    .

---
+++ scripts/run-android.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export NODE_OPTIONS='--max_old_space_size=2048'
+
+cd ~/git/distrochess
+
+echo ///////////////////////////////////
+adb devices
+# if no devices, then do:
+#	adb  kill-server
+#	adb start-server
+echo ///////////////////////////////////
+
+meteor npm install --save
+
+settingsFile=~/distrochess/settings-prod.json
+
+meteor run android-device -s $settingsFile
