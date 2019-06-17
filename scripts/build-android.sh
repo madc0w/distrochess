328d6d4be3ab756ec87aec5e2bed26d7b232c186
Marc A. Donis <marc.a.donis@primeresults.lu>
2019-06-17 18:00:47 +0200

    .

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -13,6 +13,7 @@ meteor build --verbose ~/distrochess-build --server=https://www.distrochess.com
 cd ~/git/distrochess/.meteor/local/cordova-build/platforms/android/app/build/outputs/apk/release
 rm signed.apk
 
+# keytool -genkey -alias distrochess -keyalg RSA -keysize 2048 -validity 10000
 jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.digicert.com -digestalg SHA1 app-release-unsigned.apk distrochess
 
 $ANDROID_HOME/build-tools/23.0.1/zipalign 4 app-release-unsigned.apk signed.apk
