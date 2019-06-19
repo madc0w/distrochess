556e3a8b3be10b0b8edbcc750a1b74d8309be8c4
Marc A. Donis <mad7@runbox.com>
2019-06-19 20:59:27 +0200

    .

--- client/js/signin.js
+++ client/js/signin.js
@@ -35,23 +35,23 @@ Template.signin.events({
 	},
 
 	"click #signin-google-button" : function(e) {
-		if (Meteor.isCordova) {
-			// TODO Google broke the internet.  again.
-			// see https://stackoverflow.com/questions/56665019/meteor-apk-build-broken-with-accounts-google
-			message.set("Google has broken this feature.<br/>You can still sign in with Google in the webapp, or create a Distrochess account, or sign in with Github.<br/>We are working on a solution!");
-		} else {
-			isSpinner.set(true);
-			Meteor.loginWithGoogle({
-				requestPermissions : [ "email" ]
-			}, (err) => {
-				isSpinner.set(false);
-				if (err) {
-					message.set(err.reason);
-				} else {
-					dialog.set(null);
-				}
-			});
-		}
+		//		if (Meteor.isCordova) {
+		//			// TODO Google broke the internet.  again.
+		//			// see https://stackoverflow.com/questions/56665019/meteor-apk-build-broken-with-accounts-google
+		//			message.set("Google has broken this feature.<br/>You can still sign in with Google in the webapp, or create a Distrochess account, or sign in with Github.<br/>We are working on a solution!");
+		//		} else {
+		isSpinner.set(true);
+		Meteor.loginWithGoogle({
+			requestPermissions : [ "email" ]
+		}, (err) => {
+			isSpinner.set(false);
+			if (err) {
+				message.set(err.reason);
+			} else {
+				dialog.set(null);
+			}
+		});
+	//		}
 	},
 
 	"click #signin-github-button" : function(e) {
