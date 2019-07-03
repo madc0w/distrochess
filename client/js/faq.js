477108b566094c71dfd8c4b8a723a5b12fd4d036
Marc A. Donis <mad7@runbox.com>
2019-07-03 16:25:41 +0200

    discovered "translations", which simplifies FAQ generations somewhat

--- client/js/faq.js
+++ client/js/faq.js
@@ -6,20 +6,14 @@ Template.faq.helpers({
 			initialRating : INITIAL_RATING,
 		};
 
-		var i = 0;
-		var isStop = false;
-		do {
-			i++;
+		for (var i = 1; translations.project["faq_q" + i]; i++) {
 			const question = TAPi18n.__("faq_q" + i);
-			isStop = question == "faq_q" + i;
-			if (!isStop) {
-				const answer = TAPi18n.__("faq_a" + i, values);
-				entries.push({
-					question : question,
-					answer : answer
-				});
-			}
-		} while (!isStop);
+			const answer = TAPi18n.__("faq_a" + i, values);
+			entries.push({
+				question : question,
+				answer : answer
+			});
+		}
 		return entries;
 	},
 });
